370f607d7486cac0a8fe7e52ce5678ff17d606ca
kangyeson <yesonkang@gmail.com>
2026-06-27 19:32:29 +0900

    Initial project setup

---
+++ src/test/java/com/example/jejugilmoa/JejugilmoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jejugilmoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JejugilmoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
