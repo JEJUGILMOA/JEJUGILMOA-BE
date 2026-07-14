2cbdb2184da5d5f418ecaef0ca211b3508bcd14b
kangyeson <yesonkang@gmail.com>
2026-07-14 23:01:27 +0900

    [Chore] 자동 빈 등록을 위해 @ConfigurationPropertiesScan 어노테이션 추가

--- src/main/java/com/example/jejugilmoa/JejugilmoaApplication.java
+++ src/main/java/com/example/jejugilmoa/JejugilmoaApplication.java
@@ -2,9 +2,13 @@ package com.example.jejugilmoa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 // JPA Auditing 활성화는 JpaConfig에서 담당 (중복 선언 시 jpaAuditingHandler 빈 충돌)
 @SpringBootApplication
+@ConfigurationPropertiesScan
+@EnableScheduling
 public class JejugilmoaApplication {
 
 	public static void main(String[] args) {
