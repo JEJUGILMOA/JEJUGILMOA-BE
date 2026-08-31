86e73c0cb24d65aaeb5bb71ffd04fcb623a81d36
kangyeson <yesonkang@gmail.com>
2026-08-31 20:31:20 +0900

    [Fix] 장소 동기화 데이터로 인한 통합 테스트 격리 문제 수정

--- src/test/java/com/example/jejugilmoa/domain/place/service/PlaceQueryServiceIntegrationTest.java
+++ src/test/java/com/example/jejugilmoa/domain/place/service/PlaceQueryServiceIntegrationTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
@@ -17,6 +18,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
+@Sql(statements = "TRUNCATE place CASCADE", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 class PlaceQueryServiceIntegrationTest {
 
     private static final String CATEGORY = "장소검색통합테스트";
